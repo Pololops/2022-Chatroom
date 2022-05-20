094b25d2246a2969c43e6b1dacfecaac141b81bc
Pololops <paul.rigaudeau@me.com>
2022-05-20 16:11:01 +0200

    add a middleware logger to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from '../reducers';
+import { createStore, applyMiddleware, compose } from '@reduxjs/toolkit';
+import reducer from 'src/reducers';
+import logger from 'src/middlewares/logger';
+
+// ? Commande pour permettre à l'extension Chrome Redux DevTools de se connecter à notre app
+// * Si la fonction __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ existe, alors on connecte le Redux Devtools
+// * avec la prise en charge des autres enhancers(ex: Middleware)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // ? Le store est le gardien du state dans Redux, un objet qui contient 5 méthodes, dont :
 // * getState() : permet de récupérer le state
 // * dispatch() : permet de modifier le state
 // * subscribe() : permet de s'abonner aux changements de state
-const store = configureStore(
-  {
-    reducer,
-  },
-
-  // ? Commande pour permettre à l'extension Chrome Redux DevTools de se connecter à notre app
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
